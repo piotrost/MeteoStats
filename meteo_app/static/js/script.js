71d58b02eb19553a320676950acf339ee157de55
xr3mo <remigiusz.szewczak@gmail.com>
2025-01-02 22:22:31 +0100

    Create script.js

---
+++ meteo_app/static/js/script.js
@@ -0,0 +1,124 @@
+var jednTeryt = {
+  "Dolnośląskie": [
+    "bolesławiecki", "dzierżoniowski", "głogowski", "górowski", "jaworski", "Jelenia Góra",
+    "jeleniogórski", "kamiennogórski", "kłodzki", "Legnica", "legnicki", "lubański",
+    "lubiński", "lwówecki", "milicki", "oleśnicki", "oławski",
+    "polkowicki", "strzeliński", "średzki", "świdnicki", "trzebnicki", "Wałbrzych",
+    "wałbrzyski", "wołowski", "Wrocław", "wrocławski", "ząbkowicki",
+    "zgorzelecki", "złotoryjski"
+
+  ],
+  "Kujawsko-Pomorskie": [
+    "aleksandrowski", "brodnicki", "bydgoski", "Bydgoszcz", "chełmiński", "golubsko-dobrzyński",
+    "Grudziądz", "grudziądzki", "inowrocławski", "lipnowski", "mogileński", "nakielski",
+    "radziejowski", "rypiński", "sępoleński", "świecki", "Toruń", "toruński",
+    "tucholski", "wąbrzeski", "Włocławek", "włocławski", "żniński"
+  ],
+  "Lubelskie": [
+    "bialski", "Biała Podlaska", "biłgorajski", "Chełm", "chełmski", "hrubieszowski", "janowski",
+    "krasnostawski", "kraśnicki", "lubartowski", "lubelski", "Lublin", "łęczyński",
+    "łukowski", "opolski", "parczewski", "puławski", "radzyński",
+    "rycki", "świdnicki", "tomaszowski", "włodawski", "zamojski", "Zamość"
+  ],
+  "Lubuskie": [
+    "gorzowski", "Gorzów Wielkopolski", "krośnieński", "międzyrzecki", "nowosolski", "słubicki",
+    "strzelecko-drezdenecki", "sulęciński", "świebodziński", "wschowski",
+    "Zielona Góra", "zielonogórski", "żagański", "żarski"
+  ],
+  "Łódzkie": [
+    "bełchatowski", "brzeziński", "kutnowski", "łaski", "łęczycki",
+    "łowicki", "łódzki wschodni", "Łódź", "opoczyński", "pabianicki", "pajęczański",
+    "piotrkowski", "Piotrków Trybunalski", "poddębicki", "radomszczański", "rawski",
+    "sieradzki",  "Skierniewice", "skierniewicki", "tomaszowski", "wieluński",
+    "wieruszowski", "zduńskowolski", "zgierski"
+  ],
+  "Małopolskie": [
+    "bocheński", "brzeski", "chrzanowski", "dąbrowski", "gorlicki", "krakowski", "Kraków",
+    "limanowski", "miechowski", "myślenicki", "nowosądecki", "nowotarski", "Nowy Sącz",
+    "olkuski", "oświęcimski", "proszowicki", "suski", "tarnowski", "Tarnów", "tatrzański",
+    "wadowicki", "wielicki"
+  ],
+  "Mazowieckie": [
+    "białobrzeski", "ciechanowski", "garwoliński", "gostyniński", "grodziski",
+    "grójecki", "kozienicki", "legionowski", "lipski", "łosicki", "makowski", "miński",
+    "mławski", "nowodworski", "ostrołęcki", "Ostrołęka", "ostrowski", "otwocki",
+    "piaseczyński", "Płock", "płocki", "płoński", "pruszkowski", "przasnyski",
+    "przysuski", "pułtuski", "Radom", "radomski", "Siedlce", "siedlecki", "sierpecki", "sochaczewski",
+    "sokołowski", "szydłowiecki", "Warszawa", "warszawski zachodni", "węgrowski",
+    "wołomiński", "wyszkowski", "zwoleński", "żuromiński", "żyrardowski"
+  ],
+  "Opolskie": [
+    "brzeski", "głubczycki", "kędzierzyńsko-kozielski", "kluczborski",
+    "krapkowicki", "namysłowski", "nyski", "oleski", "Opole", "opolski", "prudnicki",
+    "strzelecki"
+  ],
+  "Podkarpackie": [
+    "bieszczadzki", "brzozowski", "dębicki", "jarosławski", "jasielski",
+    "kolbuszowski", "Krosno", "krośnieński", "leski", "leżajski", "lubaczowski", "łańcucki",
+    "mielecki", "niżanski", "przemyski", "Przemyśl", "przeworski", "ropczycko-sędziszowski",
+    "rzeszowski", "Rzeszów", "sanocki", "stalowowolski", "strzyżowski", "Tarnobrzeg", "tarnobrzeski"
+  ],
+  "Podlaskie": [
+    "augustowski", "białostocki", "Białystok", "bielski", "grajewski", "hajnowski",
+    "kolneński", "Łomża", "łomżyński", "moniecki", "sejneński", "siemiatycki",
+    "sokólski", "suwalski", "Suwałki", "wysokomazowiecki", "zambrowski"
+  ],
+  "Pomorskie": [
+    "bytowski", "chojnicki", "człuchowski", "Gdańsk", "gdański", "Gdynia", "kartuski",
+    "kościerski", "kwidzyński", "lęborski", "malborski", "nowodworski",
+    "pucki", "Słupsk", "słupski", "Sopot", "starogardzki", "sztumski", "tczewski", "wejherowski"
+  ],
+  "Śląskie": [
+    "będziński", "bielski", "Bielsko-Biała", "bieruńsko-lędziński", "Bytom", "Chorzów",
+    "cieszyński", "Częstochowa", "częstochowski", "Dąbrowa Górnicza", "Gliwice", "gliwicki",
+    "Jastrzębie-Zdrój", "Jaworzno", "Katowice", "kłobucki", "lubliniecki", "mikołowski",
+    "Mysłowice", "myszkowski", "Piekary Śląskie", "pszczyński", "raciborski", "Ruda Śląska", "rybnicki",
+    "Rybnik", "Siemianowice Śląskie", "Sosnowiec", "Świętochłowice", "tarnogórski",
+    "Tychy", "wodzisławski", "Zabrze", "zawierciański", "Żory", "żywiecki"
+  ],
+  "Świętokrzyskie": [
+    "buski", "jędrzejowski", "kazimierski", "Kielce", "kielecki", "konecki",
+    "opatowski", "ostrowiecki", "pińczowski", "sandomierski",
+    "skarżyski", "starachowicki", "staszowski", "włoszczowski"
+  ],
+  "Warmińsko-Mazurskie": [
+    "bartoszycki", "braniewski", "działdowski", "Elbląg", "elbląski", "ełcki",
+    "giżycki", "gołdapski", "iławski", "kętrzyński", "lidzbarski",
+    "mrągowski", "nidzicki", "nowomiejski", "olecki", "Olsztyn", "olsztyński",
+    "ostródzki", "piski", "szczycieński", "węgorzewski"
+  ],
+  "Wielkopolskie": [
+    "chodzieski", "czarnkowsko-trzcianecki", "gnieźnieński", "gostyński",
+    "grodziski", "jarociński", "kaliski", "Kalisz", "kępiński", "kolski",
+    "Konin", "koniński", "kościański", "krotoszyński", "leszczyński",
+    "Leszno", "międzychodzki", "nowotomyski", "obornicki", "ostrowski",
+    "ostrzeszowski", "pilski", "pleszewski", "Poznań", "poznański", "rawicki",
+    "słupecki", "szamotulski", "średzki", "śremski", "turecki", "wągrowiecki",
+    "wolsztyński", "wrzesiński", "złotowski"
+  ],
+  "Zachodniopomorskie": [
+    "białogardzki", "choszczeński", "drawski", "goleniowski", "gryficki",
+    "gryfiński", "kamieński", "kołobrzeski", "Koszalin", "koszaliński", "łobeski",
+    "myśliborski", "policki", "pyrzycki", "sławieński", "stargardzki", "Szczecin",
+    "szczecinecki", "świdwiński", "Świnoujście", "wałecki"
+  ]
+};
+
+window.onload = function() {
+  var wojSel = document.getElementById("woj");
+  var powSel = document.getElementById("pow");
+
+  for (var woj in jednTeryt) {
+    wojSel.options[wojSel.options.length] = new Option(woj, woj);
+  }
+
+  wojSel.onchange = function() {
+    powSel.length = 1;
+    var pow = jednTeryt[this.value];
+    if (pow) {
+      pow.forEach(function(pow) {
+        powSel.options[powSel.options.length] = new Option(pow, pow);
+      });
+    }
+  };
+};
